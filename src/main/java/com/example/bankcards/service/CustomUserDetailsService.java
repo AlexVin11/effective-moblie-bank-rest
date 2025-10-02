3bbfb2227815dc4311de6d2a776cee077af9027f
Alexander Vinogradow <alukard981@gmail.com>
2025-10-02 13:38:19 +0300

    feat: создан класс для поиска пользователя из UserDetailsService

---
+++ src/main/java/com/example/bankcards/service/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.example.bankcards.service;
+
+import com.example.bankcards.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public final class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        var user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User with " + email + " not found"));
+        return user;
+    }
+}
