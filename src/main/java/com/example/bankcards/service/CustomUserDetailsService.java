94f19f69c14b56e834d19405329fcd5be285146d
Alexander Vinogradow <alukard981@gmail.com>
2025-10-02 13:44:38 +0300

    feat: создан кастомный класс для обработки UserDetailsService

--- src/main/java/com/example/bankcards/service/CustomUserDetailsService.java
+++ src/main/java/com/example/bankcards/service/CustomUserDetailsService.java
@@ -3,14 +3,14 @@ package com.example.bankcards.service;
 import com.example.bankcards.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public final class CustomUserDetailsService implements UserDetailsService {
+public final class CustomUserDetailsService implements UserDetailsManager {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
@@ -19,5 +19,30 @@ public final class CustomUserDetailsService implements UserDetailsService {
         var user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User with " + email + " not found"));
         return user;
+    }
+
+    @Override
+    public void createUser(UserDetails userData) {
+        throw new UnsupportedOperationException("Unimplemented method 'createUser'");
+    }
+
+    @Override
+    public void updateUser(UserDetails user) {
+        throw new UnsupportedOperationException("Unimplemented method 'updateUser'");
+    }
+
+    @Override
+    public void deleteUser(String username) {
+        throw new UnsupportedOperationException("Unimplemented method 'deleteUser'");
+    }
+
+    @Override
+    public void changePassword(String oldPassword, String newPassword) {
+        throw new UnsupportedOperationException("Unimplemented method 'changePassword'");
+    }
+
+    @Override
+    public boolean userExists(String username) {
+        throw new UnsupportedOperationException("Unimplemented method 'userExists'");
     }
 }
